ef472fb737dea9908918591586f8d0e71ecb80aa
sazzad5232 <tahibrahman@gmail.com>
2025-03-20 21:02:24 +0000

    class6&7 conversion & string number

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+ let score = "33abc"
+
+ console.log(typeof score);
+ console.log(typeof (score));
+
+ let valueInNumber = Number (score)
+
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber)
+
+ //"33" => 33
+ //"33abc" => NaN
+ //true => 1; false=> 0
+
+ let isLoggedIn="sazzad"
+ let booleanIsloggedIn = Boolean (isLoggedIn)
+ console.log(booleanIsloggedIn)
+ console.log(typeof booleanIsloggedIn)
+
+ //1 => true; 0 => false
+ //"" => false
+ //"sazzad" => true
+
+ let someNumber = 33
+ let stringNumber = String (someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
+ //************* Operation ************/
+
+ let value = 3
+ let negValue = -value
+ console.log(negValue)
+
+ //console.log(2+2)
+ //console.log(2-2)
+ //console.log(2/2)
+ //console.log(2*2)
+
+ let str1 = "hello"
+ let str2 = " sazzad"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+ 2); //12
+console.log(1+ "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32 don't write code this way
+
+console.log( ( 3 + 4 ) * 5 % 3 ); //write this way
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter) // 101
+
+//link to study ECMA Scripts.
+//https://tc39.es/ecma262/#sec-abstract-operations//
+
+
+
+
+
+
