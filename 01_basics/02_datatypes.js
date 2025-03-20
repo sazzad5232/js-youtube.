fa861d646409e73269113652210b19fee4963d05
sazzad5232 <tahibrahman@gmail.com>
2025-03-20 18:59:41 +0000

    class-no.5 datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+
+//aleart (3+3)// we are using nodejs, not browser
+
+console.log(3+3)
+console.log("sazzad")
+
+let name = "sazzad"
+let age = 18
+let isLoggedIn = true
+let state;
+
+//number => 2 power 53//
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefine => 
+
+console.log(typeof"sazzad");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefine
+
+    
