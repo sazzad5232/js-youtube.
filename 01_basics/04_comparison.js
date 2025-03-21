e2ab2464b84be7bd04d233c91f694ca9ae6cf6d4
sazzad5232 <tahibrahman@gmail.com>
2025-03-21 18:59:16 +0000

    video8 comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log(null>0); //false
+console.log(null==0);//false
+console.log(null>=0);//true because comparisons convert null to a number
+
+console.log(undefined==0) //false
+console.log(undefined<0) //false
+console.log(undefined>=0) //false (6-12 porjonto evabe code likha uchit na) always clean code likhte hobe. 
+
+console.log("2"===2) //false because (===) strickly check kore ar 2 string er modhe ache tai.
