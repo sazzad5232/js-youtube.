27db0061101169954fcfb3b5ed46a2949207d8c0
sazzad5232 <tahibrahman@gmail.com>
2025-03-22 19:18:09 +0000

    class09datatypes

---
+++ 01_basics/05datatypeshts.js
@@ -0,0 +1,16 @@
+// primitive data types
+// 7 types- string, number, boolean, null, undefined, symbol, BigInt.
+
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId);
+
+//reference (non primitive)
+//array, objects(know well if you master at JS), function.
