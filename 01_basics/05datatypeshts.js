ff9e5ad1f73495215943f5fe5ae8d745e260ba9c
sazzad5232 <tahibrahman@gmail.com>
2025-04-02 18:38:19 +0000

    datatypesummary

--- 01_basics/05datatypeshts.js
+++ 01_basics/05datatypeshts.js
@@ -11,6 +11,22 @@ let userEmail;
 const id = Symbol("123")
 const anotherId = Symbol("123")
 console.log(id === anotherId);
+const bigNumber = 345678952n
+
 
 //reference (non primitive)
 //array, objects(know well if you master at JS), function.
+
+const heros = ["batman","uperman", "spiderman"] //arrays//
+
+let myObj = {
+    name:"sazzad",
+    age:22,
+
+} //objects
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
