9800b42839b9ccaefc642eebac4b6b02deda8997
sazzad5232 <tahibrahman@gmail.com>
2025-04-04 19:00:32 +0000

    strings half

---
+++ 01_basics/06strings.js
@@ -0,0 +1,15 @@
+const name = "sazzad"
+const repoCount = 52
+
+//console.log(name+repoCount+"value") //don't use it//
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`) // use this way //
+
+const gameName = new String("sazzadrh")
+console.log(gameName[0]) //s//
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(5)) //d//
+console.log(gameName.indexOf("d")) //5//
+
