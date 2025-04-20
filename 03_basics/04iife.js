9a1b620a802db54fc62f7077ac4d9cb7f294f3c0
sazzad5232 <tahibrahman@gmail.com>
2025-04-20 09:34:47 +0000

    iife

---
+++ 03_basics/04iife.js
@@ -0,0 +1,24 @@
+// Immediately invoked function expressions (iife) //
+function chai(){
+    console.log(`DB CONNECTED`)
+}
+
+// chai() // DB CONNECTED
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+})();  //DB CONNECTED
+
+(function aur(){
+    // function aur eta holo named iife.
+    console.log(`DB CONNECTED TWO`)
+})(); // DB CONNECTED TWO,, ei ans asbe na jodi uporer ei code })() er por.
+
+(()=>{
+    console.log(`DB CONNECTED TWO`)
+})(); //  DB CONNECTED TWO,, ei ans asbe na jodi uporer ei code })() er por.
+
+((name)=>{
+    // name holo unnamed iife.
+    console.log(`DB CONNECTED TWO ${name}`)
+})("hitesh")  //  DB CONNECTED TWO hitesh. eivabe o excicute kora jai.
