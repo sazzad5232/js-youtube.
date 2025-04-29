73fb0209d4047dc708fc577f05fed18f04e865f2
sazzad5232 <tahibrahman@gmail.com>
2025-04-29 09:21:49 +0000

    25 & 24 video

--- 03_basics/04iife.js
+++ 03_basics/04iife.js
@@ -19,6 +19,8 @@ function chai(){
 })(); //  DB CONNECTED TWO,, ei ans asbe na jodi uporer ei code })() er por.
 
 ((name)=>{
-    // name holo unnamed iife.
+    // name holo unnamed iife.   
     console.log(`DB CONNECTED TWO ${name}`)
 })("hitesh")  //  DB CONNECTED TWO hitesh. eivabe o excicute kora jai.
+
+// video 25 holo practicle ja video dekha sikhte hobe //
