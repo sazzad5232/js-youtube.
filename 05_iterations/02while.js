d81b5e8e8848ae3329dc99356000bc5576963657
sazzad5232 <tahibrahman@gmail.com>
2025-05-14 14:15:54 +0000

    do while

---
+++ 05_iterations/02while.js
@@ -0,0 +1,25 @@
+// video28
+let index=0;
+while (index<=10) {
+    console.log(`Value of index is ${index}`); //Value of index is 0,Value of index is 2,Value of index is 4--10
+    index = index +2;
+}
+
+let myArr = ["flash","batman","superman"];
+let arr = 0
+while (arr<myArr.length) {
+   console.log(`Value is ${myArr[arr]}`) //Value is flash,value is batman,value is superman.
+   arr = arr +1
+}
+//do while
+// let score = 1;
+// do {
+//     console.log(`score is ${score}`); // score is 1,score is 2,score is 3---score is 10.
+//     score++
+// } while (score<=10);
+
+let score = 11;
+do {
+    console.log(`score is ${score}`); // score is 11.
+    score++
+} while (score<=10);
